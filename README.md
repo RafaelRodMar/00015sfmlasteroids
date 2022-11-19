c256b83f1f84dc6ba1c36446afc62c1cb9db01cf
Rafael Rodríguez Martínez <48961409+RafaelRodMar@users.noreply.github.com>
2022-11-19 19:36:40 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# 00015sfmlasteroids
-Asteroids game made with SFML.
+# asteroids
+Asteroids game.
+
+Keys : arrows move the ship, space shoots and 'z' is the shield.
+
+Credit for the original code to FamTrinli ( https://www.youtube.com/watch?v=rWaSo2usU4A&t=5s ) 
+Take a look at all the 16 games he did.
---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <SFML/Graphics.hpp>
+#include <sfml/audio.hpp>
+#include <vector>
+#include <list>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <time.h>
+#include <cstring>
+#include <random>
+#include <chrono>
+
+#include "various.h"
+#include "animation.h"
+#include "entity.h"
+#include "game.h"
+
+int main()
+{
+    //the game class
+    Game game;
+
+    //create the main window
+    game.init(1200,800,200,0,"Asteroids");
+
+    ReadHiScores(game.vhiscores);
+
+    sf::Clock clock;
+    const sf::Time timePerFrame = sf::seconds(1.f/60.f); //60fps
+    sf::Time elapsed = sf::Time::Zero;
+
+    while(game.app.isOpen())
+    {
+        elapsed += clock.restart();
+
+        game.input();
+
+        while( elapsed >= timePerFrame )
+        {
+            game.update(elapsed);
+            elapsed -= timePerFrame;
+        }
+
+        game.draw();
+    }
+
+    WriteHiScores(game.vhiscores);
+
+    return EXIT_SUCCESS;
+}
+
